b134d707b1bbcb8c131530acac67178f65c55be4
prasad barhate <prasadbarhate388@gmail.com>
2026-06-24 01:01:59 +0530

    added health route,

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/",(req,res) => {
     return res.send("hello world")
 })
 
+app.get("/heath",(req,res) => {
+    return res.send("everything is fine")
+})
+
 app.listen(port,() => {
     console.log(`server is running at port ${port}`)
 })
